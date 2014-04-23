b1fce59e04378cc5021f6d850f91d92c914be1d0
JayK31 <jay.kaye31@gmail.com>
2014-04-23 09:42:05 -0400

    second commit

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,4 +1,8 @@
 class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+  end
+
   def new
     @cat = Cat.new
   end
@@ -10,7 +14,21 @@ class CatsController < ApplicationController
       else
         render :new
       end
+    end
   end
+
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    cat = Cat.find(params[:id])
+
+    cat.update(cat_params)
+
+    redirect_to cat_path(cat)
+  end
+
 
   private
 
